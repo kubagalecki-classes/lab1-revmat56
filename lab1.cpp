b11e90bb3e44c5bbdb36d2f3670a51572b2873cc
revmat56 <62846638+revmat56@users.noreply.github.com>
2020-10-29 13:30:00 +0100

    Update lab1.cpp

--- lab1.cpp
+++ lab1.cpp
@@ -1,6 +1,20 @@
 #include <iostream>
+#include <cmath>
+
+struct wektor
+{
+    void drukuj_wektor() { std::cout << "Wspolrzedne wektora, x: " << x << " y: " << y << "\n" ; }
+    double x;
+    double y;
+    double norm() { return sqrt(x*x+y*y);}
+};
 
 int main()
 {
     puts("Hello, World!");
+    wektor r;
+    r.x = 1.5;
+    r.y = 2.85;
+    r.drukuj_wektor();
+    std::cout << "Dlugosc wektora:" << r.norm();
 }
