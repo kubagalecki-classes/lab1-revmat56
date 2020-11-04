83f54b2d3c6f4d8de8af61940de73bcf3df127c7
revmat56 <62846638+revmat56@users.noreply.github.com>
2020-11-04 23:24:49 +0100

    Update Wektor2D.hpp

--- zal/Wektor2D.hpp
+++ zal/Wektor2D.hpp
@@ -1,4 +1,24 @@
 class Wektor2D
 {
-    // Tutaj napisz implementacje klasy Wektor2D
+    Wektor2D() { xcoor = 0.; ycoor = 0.;}
+    Wektor2D (double x, double y)
+    {
+        xcoor = x;
+        ycoor = y;
+    }
+  
+    void setX(int a) { xcoor = a; }
+    int  getY()      { return xcoor; }
+    void setX(int b) { ycoor = b; }
+    int  getY()      { return ycoor; }
+ 
+    Wektor2D operator+(Wektor2D w1, Wektor w2)
+    {
+    return Wektor2D{w1, w2}
+    }
+    
+
+    
+    double  xcoor;
+    double  ycoor;
 };
